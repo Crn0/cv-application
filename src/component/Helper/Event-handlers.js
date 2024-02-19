aafd0cfd5a7d015f8c8b81e51df4bfd53e88c30d
EssenceCode <123456789+odin@users.noreply.github.com>
2024-02-19 14:26:32 +0800

    Remove verbose key value

--- src/component/Helper/Event-handlers.js
+++ src/component/Helper/Event-handlers.js
@@ -1,44 +1,44 @@
 // personal info events
 export function HandleFirstName(e, personName,setName) {
-    setName({...personName, firstName: e.target.value, lastName: personName.lastName});
+    setName({...personName, firstName: e.target.value});
 };
 
 export function HandleLastName(e, personName,setName) {
-    setName({...personName, firstName: personName.firstName, lastName: e.target.value});
+    setName({...personName, lastName: e.target.value});
 };
 
 export function HandleEmail(e, contactInfo, setContactInfo) {
-    setContactInfo({contactInfo, email: e.target.value, phoneNumber: contactInfo.phoneNumber});
+    setContactInfo({contactInfo, email: e.target.value});
 };
 
 export function HandlePhoneNumber(e, contactInfo, setContactInfo) {
-    setContactInfo({contactInfo, email: contactInfo.email, phoneNumber: e.target.value});
+    setContactInfo({contactInfo, phoneNumber: e.target.value});
 };
 
 // school events
 export function HandleSchoolName(e, schoolInfo, setSchoolInfo) {
-    setSchoolInfo({...schoolInfo, name: e.target.value, degree: schoolInfo.degree});
+    setSchoolInfo({...schoolInfo, name: e.target.value});
 };
 
 export function HandleSchoolDegree(e, schoolInfo, setSchoolInfo) {
-    setSchoolInfo({...schoolInfo, name: schoolInfo.name, degree: e.target.value});
+    setSchoolInfo({...schoolInfo, degree: e.target.value});
 };
 
 export function HandleSchoolStart(e, schooling, setSchooling) {
-    setSchooling({schooling, start: e.target.value, end: schooling.end});
+    setSchooling({schooling, start: e.target.value});
 };
 
 export function HandleSchoolEnd(e, schooling, setSchooling) {
-    setSchooling({schooling, start: schooling.start, end: e.target.value});
+    setSchooling({schooling, end: e.target.value});
 };
 
 // work events
 export function HandleCompanyName(e, companyInfo, setCompanyInfo) {
-    setCompanyInfo({...companyInfo, name: e.target.value, job: companyInfo.job});
+    setCompanyInfo({...companyInfo, name: e.target.value});
 };
 
 export function HandleCompanyJob(e, companyInfo, setCompanyInfo) {
-    setCompanyInfo({...companyInfo, name: companyInfo.name, job: e.target.value});
+    setCompanyInfo({...companyInfo, job: e.target.value});
 };
 
 export function HandleResponsibility(e, setResponsibility) {
@@ -46,7 +46,7 @@ export function HandleResponsibility(e, setResponsibility) {
 };
 
 export function HandleJobStart(e, jobExperience, setJobExperience) {
-    setJobExperience({...jobExperience, start: e.target.value, end: jobExperience.end});
+    setJobExperience({...jobExperience, start: e.target.value});
 };
 
 export function HandleJobEnd(e, jobExperience, setJobExperience) {
